2b343a82d7bcd70a67e0108c4aa4cf4e2e90fbde
Mario Granados <mario.alberto.granados1@gmail.com>
2022-07-15 10:32:08 -0500

    front page

---
+++ frontend/src/Pages/HomePage.js
@@ -0,0 +1,15 @@
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
+import Banner from "../Components/Banner";
+
+const HomePage = () => {
+    return(
+        <>
+            <Navbar/>
+            <Banner/>
+            <Footer/>
+        </>
+    );
+
+}
+export default HomePage;
