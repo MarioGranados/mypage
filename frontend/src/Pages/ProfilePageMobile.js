b169daf2766e3db68c86f5734570114405ceb194
Mario Granados <mario.alberto.granados1@gmail.com>
2022-07-18 16:41:41 -0500

    almost done with front end

--- frontend/src/Pages/ProfilePageMobile.js
+++ frontend/src/Pages/ProfilePageMobile.js
@@ -1,4 +1,15 @@
+import {useEffect, useState} from "react";
+
 const ProfilePageMobile = () => {
+    let dog = []
+    const [URL, setURL] = useState('')
+    if(URL === '') {
+        fetch('https://dog.ceo/api/breeds/image/random')
+            .then((response) => response.json())
+            .then((json) => dog.push(json)).then(() => {
+            setURL(dog[0].message)
+        })
+    }
     const user = {
         userName: 'Mario',
         firstName: 'Mario',
@@ -8,9 +19,9 @@ const ProfilePageMobile = () => {
     }
     const links = {
         title: 'title',
-        url: 'url',
-        desc: 'desc',
-        thumbnail: 'thumbnail'
+        url: 'https://facebook.com',
+        desc: 'my personal facebook page',
+        thumbnail:  URL,
     }
     return(
         <>
@@ -18,15 +29,36 @@ const ProfilePageMobile = () => {
                 <div className="row">
 
                 </div>
-                <div className="row">
-                    <a href="#" className="btn btn-outline-light mt-3">{links.title}</a>
-                    <a href="#" className="btn btn-outline-light mt-3">{links.title}</a>
-                    <a href="#" className="btn btn-outline-light mt-3">{links.title}</a>
-                    <a href="#" className="btn btn-outline-light mt-3">{links.title}</a>
-                    <a href="#" className="btn btn-outline-light mt-3">{links.title}</a>
+                <a href={links.url}><div className="row">
+                    <div className="card mb-3 bg-dark text-white" >
+                        <div className="row g-0">
+                            <div className="col-md-2">
+                                <img src={links.thumbnail} className="img-fluid rounded-start" alt={links.title + ' link'}/>
+                            </div>
+                            <div className="col-md-8">
+                                <div className="card-body">
+                                    <h5 className="card-title">{links.title}</h5>
+                                    <p className="card-text">{links.desc}</p>
+                                    <p className="card-text"><small className="text-white">{links.url}</small></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="row">
-
+                </a>
+                <div className="row justify-content-around align-content-center">
+                    <div className="col">
+                        <a href="#" className="btn btn-outline-light mt-3">{links.title}</a>
+                    </div>
+                    <div className="col">
+                        <a href="#" className="btn btn-outline-light mt-3">{links.title}</a>
+                    </div>
+                    <div className="col">
+                        <a href="#" className="btn btn-outline-light mt-3">{links.title}</a>
+                    </div>
+                    <div className="col">
+                        <a href="#" className="btn btn-outline-light mt-3">{links.title}</a>
+                    </div>
                 </div>
             </div>
         </>
